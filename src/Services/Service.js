a00b22d7fa8b0bd60f613a0ba312a8bb5b553a74
Matheus Enrike Ramalho Ortiz <matheus@senai>
2023-12-11 15:06:14 -0300

    Services

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -40,13 +40,13 @@ export const institutionResource = '/Instituicao';
  */
 export const loginResource = '/Login';
 
-const apiPort = '5000';
-const localApiUri = `http://localhost:${apiPort}/api`;
-const externallApiUri = ``;
+// const apiPort = '5000';
+// const localApiUri = `http://localhost:${apiPort}/api`;
+const externallApiUri = `https://eventplus-matheusenrike.azurewebsites.net/api`;
 // const externalApiUri = null;
 
 const api = axios.create({
-    baseURL: localApiUri
+    baseURL: externallApiUri
 });
 
 
